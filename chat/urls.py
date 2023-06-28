659d0c88bd47875bb2113df3fe8fcace673e07de
JoshuaCarrera <joshuacarreraramos@gmail.com>
2023-06-27 21:09:53 -0400

    Basic structure of the website

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path((''), views.home)
+    path((''), views.home, name='home'),
+    path(('chat/<int:id>'), views.chat_interaction_view, name='chat_interaction_view')
 ]
--- chat/views.py
+++ chat/views.py
@@ -5,3 +5,9 @@ from .models import Profile, Chat, Message
 def home(request):
     context = {'available_chats': list(Chat.objects.values())}
     return render(request, 'index.html', context)
+
+def chat_interaction_view(request, id):
+    messages = Message.objects.filter(chat=id)
+    name_chat = Chat.objects.get(id=id).name
+    context = {'messages': messages, 'name_chat': name_chat}
+    return render(request, 'chat.html', context)
