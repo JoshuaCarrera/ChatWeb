191c183f837b06ba1ab7a5f74ea13b15176ab29f
JoshuaCarrera <joshuacarreraramos@gmail.com>
2023-06-28 19:55:57 -0400

    Added Message form

--- chat/views.py
+++ chat/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render
 from .models import Profile, Chat, Message
+from .forms import MessageForm
 
 # Create your views here.
 def home(request):
     context = {'available_chats': list(Chat.objects.values())}
     return render(request, 'index.html', context)
 
-def chat_interaction_view(request, id):
-    messages = Message.objects.filter(chat=id)
-    name_chat = Chat.objects.get(id=id).name
-    context = {'messages': messages, 'name_chat': name_chat}
-    return render(request, 'chat.html', context)
-
 def profile_view(request, id):
     profile = Profile.objects.get(id=id)
     messages_sents = Message.objects.filter(sender_id=id)
     context = {'profile': profile, 'messages_sents': messages_sents}
     return render(request, 'profile.html', context)
+
+def chat_interaction_view(request, id):
+    messages = Message.objects.filter(chat=id)
+    current_chat = Chat.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.chat = current_chat
+            message.sender = request.user.profile
+            message.save()
+            form = MessageForm()
+    else:
+        form = MessageForm()
+
+    context = {'messages': messages, 'chat': current_chat, 'form': form}
+    return render(request, 'chat.html', context)
+
+def message_send(request):
+    pass
+
+
+
