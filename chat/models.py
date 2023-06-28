50922806b79b421578018c47525a49a73d1bfde4
JoshuaCarrera <joshuacarreraramos@gmail.com>
2023-06-27 20:24:51 -0400

    Add view and template

--- chat/models.py
+++ chat/models.py
@@ -4,17 +4,23 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='profile_pics', null=True)
+    img = models.ImageField(upload_to='profile_pics', blank=True)
     def __str__(self):
         return f'{self.user.username} Profile'
 
-class Message(models.Model):
-    from_user = models.ForeignKey(User, related_name='messages_sent', on_delete=models.SET_NULL, null=True)
-    to_user = models.ForeignKey(User, related_name='messages_received', on_delete=models.SET_NULL, null=True)
-    date_sent = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
-    text = models.TextField()
-    file = models.FileField(upload_to=None, max_length=100)
+class Chat(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    users = models.ManyToManyField(Profile, related_name='chats')
     def __str__(self):
-        return f'{self.data_sent} | from {self.from_user.username} to {self.to_user.username}'
+        return self.name
+
+class Message(models.Model):
+    sender = models.ForeignKey(Profile, related_name='messages_sent', on_delete=models.SET_NULL, null=True)
+    chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE, default=None)
+    date_sent = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    file = models.FileField(upload_to='chat_files', null=True)
+    def __str__(self):
+        return f'{self.date_sent} | from {self.sender.username} to {self.chat.name}'
     
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Profile, Chat, Message
 
 # Create your views here.
+def home(request):
+    context = {'available_chats': list(Chat.objects.values())}
+    return render(request, 'index.html', context)
