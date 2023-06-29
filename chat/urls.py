33609ce718d1580186626c463000911d4a525237
JoshuaCarrera <joshuacarreraramos@gmail.com>
2023-06-28 21:12:00 -0400

    Added signup, signin and signout

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path((''), views.home, name='home'),
+    path(('signup/'), views.signup, name='signup'),
+    path(('signin/'), views.signin, name='signin'),
+    path(('signout/'), views.signout, name='signout'),    
     path(('chat/<int:id>'), views.chat_interaction_view, name='chat_interaction_view'),
     path(('profile/<int:id>'), views.profile_view, name='profile_view'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from .models import Profile, Chat, Message
 from .forms import MessageForm
 
@@ -13,10 +17,43 @@ def profile_view(request, id):
     context = {'profile': profile, 'messages_sents': messages_sents}
     return render(request, 'profile.html', context)
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+            profile = Profile.objects.create(user=user)
+            user.save()
+            profile.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm
+    
+    context = {'form': form}
+    return render(request, 'signup.html', context)
+
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'POST':
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
+        login(request, user)
+        return redirect('home')
+    else:
+        return render(request, 'signin.html', {"form": AuthenticationForm})
+
+@login_required
 def chat_interaction_view(request, id):
     messages = Message.objects.filter(chat=id)
     current_chat = Chat.objects.get(id=id)
-
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
@@ -27,12 +64,5 @@ def chat_interaction_view(request, id):
             form = MessageForm()
     else:
         form = MessageForm()
-
     context = {'messages': messages, 'chat': current_chat, 'form': form}
     return render(request, 'chat.html', context)
-
-def message_send(request):
-    pass
-
-
-
