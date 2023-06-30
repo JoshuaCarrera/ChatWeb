42e57c9454cb1915dbbf9c53d118d2310f3d2957
JoshuaCarrera <joshuacarreraramos@gmail.com>
2023-06-29 22:48:24 -0400

    Added detail chat view

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,32 +4,44 @@ from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_group_name = 'test'
+        chat_id = self.scope['url_route']['kwargs']['chat_id']
+        user = self.scope['user']
 
+        self.profile_name = user.username
+        self.profile_pic = user.profile.img.url
+
+        self.room_group_name = f'chat_{chat_id}'
+
+        print("You connect to " + self.room_group_name, self.profile_pic, self.profile_name)
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
         self.accept()
-   
-
+    
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        message_sender = text_data_json['sender']
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type':'chat_message',
-                'message':message
+                'message':message,
+                'sender': message_sender
             }
         )
 
     def chat_message(self, event):
         message = event['message']
+        sender = event['sender']
 
         self.send(text_data=json.dumps({
             'type':'chat',
-            'message':message
+            'message':message,
+            'sender': sender,
+            'profile_name': self.profile_name,
+            'profile_pic': self.profile_pic,
         }))
